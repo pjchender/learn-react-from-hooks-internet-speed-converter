f16b68b0499bc368bf362e28e91e40794b182717
pjchender <pjchender@gmail.com>
2020-06-17 23:58:28 +0800

    Add started template with CSS and JSX

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
+import './App.css';
+
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="card-header">Network Speed Converter</div>
+      <div className="card-body">
+        <div className="unit-control">
+          <div className="unit">Mbps</div>
+          <span className="exchange-icon fa-fw fa-stack">
+            <i className="far fa-circle fa-stack-2x"></i>
+            <i className="fas fa-exchangne-alt fa-stack-1x"></i>
+          </span>
+          <div className="unit">MB/s</div>
+        </div>
+        <div className="converter">
+          <div className="flex-1">
+            <div className="converter-title">Set</div>
+            <input type="number" className="input-number" min="0" />
+          </div>
+          <span className="angle-icon fa-2x" style={{ marginTop: 30 }}>
+            <i className="fas fa-angle-right"></i>
+          </span>
+          <div className="text-right flex-1">
+            <div className="converter-title">Show</div>
+            <input
+              type="text"
+              className="input-number text-right"
+              disabled
+              value="125"
+            />
+          </div>
+        </div>
+      </div>
+      <div className="card-footer">FAST</div>
     </div>
   );
 }
