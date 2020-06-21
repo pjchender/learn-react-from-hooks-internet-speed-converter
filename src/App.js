608a70f0ad2368312cf0d026c03294f0187be3f8
pjchender <pjchender@gmail.com>
2020-06-21 22:58:13 +0800

    Split UnitConverter into a component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import React, { useState } from 'react';
 
 import CardFooter from './components/CardFooter';
 import UnitControl from './components/UnitControl';
+import UnitConverter from './components/UnitConverter';
 
 function App() {
   const [inputValue, setInputValue] = useState(0);
@@ -18,30 +19,10 @@ function App() {
       <div className="card-header">Network Speed Converter</div>
       <div className="card-body">
         <UnitControl />
-        <div className="converter">
-          <div className="flex-1">
-            <div className="converter-title">Set</div>
-            <input
-              type="number"
-              onChange={handleInputChange}
-              value={inputValue}
-              className="input-number"
-              min="0"
-            />
-          </div>
-          <span className="angle-icon fa-2x" style={{ marginTop: 30 }}>
-            <i className="fas fa-angle-right"></i>
-          </span>
-          <div className="text-right flex-1">
-            <div className="converter-title">Show</div>
-            <input
-              type="text"
-              className="input-number text-right"
-              disabled
-              value={inputValue / 8}
-            />
-          </div>
-        </div>
+        <UnitConverter
+          inputValue={inputValue}
+          handleInputChange={handleInputChange}
+        />
       </div>
       <CardFooter inputValue={inputValue} />
     </div>
