72b38c678f44e662d0a35ccc43df28452e89163d
pjchender <pjchender@gmail.com>
2022-08-21 17:43:03 +0800

    remove importing React in components

--- src/components/CardFooter.js
+++ src/components/CardFooter.js
@@ -1,5 +1,3 @@
-import React from 'react';
-
 const CardFooter = () => <div className="card-footer">FAST</div>;
 
 export default CardFooter;
--- src/components/UnitControl.js
+++ src/components/UnitControl.js
@@ -1,5 +1,3 @@
-import React from 'react';
-
 const UnitControl = () => (
   <div className="unit-control">
     <div className="unit">Mbps</div>
